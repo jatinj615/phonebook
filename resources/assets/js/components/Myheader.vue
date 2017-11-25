4e3d1eff8ec6300cd9f2a2aa50b7c05277daa16d
Jatin <jatinj615@gmail.com>
2017-11-25 21:47:40 +0530

    modal create

--- resources/assets/js/components/Myheader.vue
+++ resources/assets/js/components/Myheader.vue
@@ -1,7 +1,7 @@
 <template>
 	<nav class="navbar" role="navigation" aria-label="main navigation">
 	  <div class="navbar-brand">
-	    <a class="navbar-item" href="https://bulma.io">
+	    <a class="navbar-item" href="#">
 	      <img alt="Phone Book" width="112" height="28">
 	    </a>
 	    <div class="navbar-menu is-active">
