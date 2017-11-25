67414f7b4e5682e3c253d31b5c5fd0312431f138
Jatin <jatinj615@gmail.com>
2017-11-25 13:37:56 +0530

    home and about

---
+++ resources/assets/js/components/Myfooter.vue
@@ -0,0 +1,11 @@
+<template>
+	<footer class="footer">
+  <div class="container">
+    <div class="content has-text-centered">
+      <p>
+        <strong>PhoneBook App</strong> by <a href="http://jatinj615.github.io" target="_blank">Jatin Jain</a>
+      </p>
+    </div>
+  </div>
+</footer>
+</template>
